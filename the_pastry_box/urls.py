e068438e6160dcae984b4cb52442f8bebd001263
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-01-15 19:32:44 +0100

    created home app

--- the_pastry_box/urls.py
+++ the_pastry_box/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/'),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
