c0290f340f4c85f23d3a5f4a668de9f2e329b45e
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-01-26 17:49:19 +0100

    created & styled footer

---
+++ static/js/scripts.js
@@ -0,0 +1,31 @@
+// FOOTER ACCORDION ON MOBILE
+$(document).ready(() => {
+  // Target the mobiles
+  if (window.innerWidth < 576) {
+    $('.footer-block-item-content').hide();
+    $('.footer-block-item-title').click((e) => {
+      let toggler = e.target;
+      let target = toggler.getAttribute('data-target');
+
+      if ($(target).is(':hidden')) {
+        $(toggler).addClass('open');
+        $(target).slideDown('fast');
+        // Switch the + icon to a - when the related content is displayed
+        if ($(toggler).hasClass('open')) {
+          $(toggler).children('.fa-plus').fadeOut(150, () => {
+            $(toggler).children('.fa-minus').fadeIn(150);
+          });
+        }
+      } else {
+        $(target).slideUp('fast');
+        $(toggler).removeClass('open');
+        // Switch the - icon to a + when the related content is hidden
+        $(toggler).children('.fa-minus').fadeOut(150, () => {
+          $(toggler).children('.fa-plus').fadeIn(150);
+        });
+      }
+    })
+  } else {
+    $('.footer-block-item-content').show();
+  }
+})
