919dca322625bf5aa0ea4f126a36a5091823e9c8
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-01-27 17:04:55 +0100

    added products views and templates + minor css

--- products/models.py
+++ products/models.py
@@ -1,4 +1,3 @@
-from tabnanny import verbose
 from django.db import models
 
 
@@ -11,10 +10,10 @@ class Category(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
         return self.friendly_name
+
+        # def get_friendly_name(self):
+        #     return self.friendly_name
 
 
 class Brand(models.Model):
@@ -22,10 +21,10 @@ class Brand(models.Model):
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name
-
-    def get_friendly_name(self):
         return self.friendly_name
+
+    # def get_friendly_name(self):
+    #     return self.friendly_name
 
 
 class Product(models.Model):
