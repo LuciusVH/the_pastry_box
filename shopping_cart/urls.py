4752e20aac1870724783ca74ccabaa511647e963
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-05 15:32:03 +0100

    added add to cart functionality

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shopping_cart, name='shopping_cart'),
+    path('', views.view_shopping_cart, name='view_shopping_cart'),
+    path('add/<product_sku>/', views.add_to_shopping_cart,
+         name='add_to_shopping_cart'),
 ]
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
-def shopping_cart(request):
-    """ Shopping cart content """
+def view_shopping_cart(request):
+    """ View the shopping cart content """
 
     return render(request, 'shopping_cart/shopping_cart.html')
+
+
+def add_to_shopping_cart(request, product_sku):
+    """ Add product x quantity to the shopping cart """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    shopping_cart = request.session.get('shopping_cart', {})
+
+    if product_sku in list(shopping_cart.keys()):
+        shopping_cart[product_sku] += quantity
+    else:
+        shopping_cart[product_sku] = quantity
+
+    request.session['shopping_cart'] = shopping_cart
+    print(request.session['shopping_cart'])
+    return redirect(redirect_url)
