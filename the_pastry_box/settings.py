0e997670e25f601aa064611fb1fdf6bf6231757f
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-11 08:35:00 +0100

    created checkout app & models

--- the_pastry_box/settings.py
+++ the_pastry_box/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xlx*v5t2o2r+m3dl4o=udohs6wx4@aj_@n56&7vd_-!6uonmkz'
+SECRET_KEY = 'django-insecure-xlx*v5t2o2r+m3dl4o=udohs6wx4@aj_@n56&7vd_-!6uonmkz' # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'shopping_cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -119,16 +120,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # noqa
     },
 ]
 
