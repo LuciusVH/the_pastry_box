05b48783f6ccab796cee3c9b086f3faa204792cf
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-11 11:16:26 +0100

    added checkout view & template

--- the_pastry_box/settings.py
+++ the_pastry_box/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'shopping_cart',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'the_pastry_box.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +80,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'shopping_cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
