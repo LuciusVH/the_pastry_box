56fbdf5fbfecd82a4a2256c339940f1258f03dcc
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-15 16:00:21 +0100

    created subscription app

---
+++ subscription/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Product
+
+# Create your views here.
+
+
+def subscription(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
