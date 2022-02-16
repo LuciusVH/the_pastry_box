9248dbf675e65f4601c234fa23191daf46c01145
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-16 16:56:08 +0100

    customizing all allauth forms

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
 
