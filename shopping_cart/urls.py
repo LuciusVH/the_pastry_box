e9f5c88cd39a395e44c3e7737b8af89042b3b1ea
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-18 11:40:39 +0100

    added buy now functionality

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.view_shopping_cart, name='view_shopping_cart'),
     path('add/<product_id>/', views.add_to_shopping_cart,
          name='add_to_shopping_cart'),
+    path('buy/<product_id>/', views.buy_now, name='buy_now'),
     path('adjust/<product_id>/', views.adjust_shopping_cart,
          name='adjust_shopping_cart'),
     path('remove/<product_id>/', views.remove_from_shopping_cart,
