ba86b5c0a29631620969527f7a8bf200a22e254c
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-20 15:55:12 +0100

    email testing

--- the_pastry_box/settings.py
+++ the_pastry_box/settings.py
@@ -110,14 +110,14 @@ SITE_ID = 1
 
 # Email
 # if not DEBUG:
-#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#     EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-#     EMAIL_HOST = 'smtp.gmail.com'
-#     EMAIL_PORT = 587
-#     EMAIL_USE_TLS = True
-#     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # else:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Authentification
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
