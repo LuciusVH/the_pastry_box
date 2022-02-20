b10389418b65b50abda5f9dfcbcade8b13111615
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-20 18:13:13 +0100

    product admin - added prod view, url & template

--- products/forms.py
+++ products/forms.py
@@ -14,8 +14,27 @@ class ProductForm(forms.ModelForm):
         c_friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
         brands = Brand.objects.all()
         b_friendly_names = [(b.id, b.get_friendly_name()) for b in brands]
+        # placeholders = {
+        #     'category': 'Category',
+        #     'name': 'Name',
+        #     'brand': 'Brand',
+        #     'sku': 'SKU',
+        #     'description': 'Description',
+        #     'price': 'Price',
+        #     'image_url': 'image_url',
+        #     'image': 'Image import',
+        # }
 
         self.fields['category'].choices = b_friendly_names
         self.fields['brand'].choices = c_friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border'
+            if field_name != 'image':
+                field.widget.attrs['class'] = 'border'
+        # for field in self.fields:
+            # if field != 'category' or field != 'brand':
+            #     if self.fields[field].required:
+            #         placeholder = f'{placeholders[field]} *'
+            #     else:
+            #         placeholder = placeholders[field]
+            # self.fields[field].widget.attrs['placeholder'] = placeholder
+            # self.fields[field].label = False
