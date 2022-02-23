d725f5ce4223ab235a7508ac0eb8f4909b73c166
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-23 16:49:26 +0100

    template & view completed

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
 
 # Create your views here.
 
 
 def contact(request):
-    return render(request, 'contact/contact.html')
+    """
+    Contact form
+    """
+
+    if request.method == 'POST':
+        contact_name = request.POST.get('contact-name')
+        contact_email = request.POST.get('contact-email')
+        contact_message = request.POST.get('contact-message')
+
+        send_mail(
+            f"You've got a message from {contact_name} ({contact_email})",
+            contact_message,
+            contact_email,
+            ['thepastrybox.ms4@gmail.com', ]
+        )
+
+        messages.success(request, f'Hey {contact_name}! Your message has been \
+                         sent 📧 We will get back to you shortly.')
+        return render(request, 'contact/contact.html')
+    else:
+        return render(request, 'contact/contact.html')
