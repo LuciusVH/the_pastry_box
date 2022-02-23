7c206f70849d95b0e3f85f035dbdf32fae664582
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-23 15:15:11 +0100

    added date fields for newsletter models

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Subscriber, NewsletterContent
 # Register your models here.
 
+
+class NewsletterContentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date',)
+    ordering = ('-date',)
+
+
 admin.site.register(Subscriber)
-admin.site.register(NewsletterContent)
+admin.site.register(NewsletterContent, NewsletterContentAdmin)
--- newsletter/models.py
+++ newsletter/models.py
@@ -5,14 +5,16 @@ from django.db import models
 
 class Subscriber(models.Model):
     email = models.EmailField()
+    date_subscription = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
 
 
 class NewsletterContent(models.Model):
-    title = models.CharField(max_length=120)
+    title = models.CharField(max_length=200)
     content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
