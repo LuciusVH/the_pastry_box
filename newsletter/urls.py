b9ee3e054448224a91365c4b167254533b6b7ee7
LuciusVH <78437386+LuciusVH@users.noreply.github.com>
2022-02-26 14:05:10 +0100

    newsletter posts displayed as recipes

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('subscription/', views.newsletter_subscription,
          name='newsletter_subscription'),
-    path('send/', views.send_newsletter,
-         name='send_newsletter'),
+    path('send/', views.send_newsletter, name='send_newsletter'),
+    path('recipes/', views.display_recipes, name='recipes'),
+    path('recipes/<int:post_id>/', views.recipe_detail, name='recipe_detail'),
 ]
